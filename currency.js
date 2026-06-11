d07fca4c6af03e3f1535ac2cc5ba13619f822d03
educationxwx-rgb <educationxwx@gmail.com>
2026-06-11 06:48:12 +0100

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,1 +1,18 @@
+function convert() {
 
+  let value =
+    Number(document.getElementById("usd").value);
+
+  if (isNaN(value) || value <= 0) {
+
+    document.getElementById("result").innerText =
+      "أدخل قيمة صحيحة";
+
+    return;
+  }
+
+  let rate = 10.2;
+
+  document.getElementById("result").innerText =
+    (value * rate).toFixed(2) + " درهم";
+}
