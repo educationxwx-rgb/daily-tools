20719ce91ea428293a2fccb45a424d3ca91fc0d8
educationxwx-rgb <educationxwx@gmail.com>
2026-06-11 06:45:10 +0100

    Update age.js

--- age.js
+++ age.js
@@ -1,1 +1,19 @@
+function age() {
 
+  let year =
+    Number(document.getElementById("year").value);
+
+  if (!year) {
+
+    document.getElementById("ageResult").innerText =
+      "أدخل سنة الميلاد";
+
+    return;
+  }
+
+  let currentYear =
+    new Date().getFullYear();
+
+  document.getElementById("ageResult").innerText =
+    (currentYear - year) + " سنة";
+}
