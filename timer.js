66f7a00a24655c5b6d1830130ec65e0b31b68a4c
educationxwx-rgb <educationxwx@gmail.com>
2026-06-11 06:52:09 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,1 +1,40 @@
+let timer;
+let remaining = 0;
 
+function startTimer() {
+
+  if (remaining <= 0) {
+    remaining =
+      Number(document.getElementById("seconds").value);
+  }
+
+  if (!remaining) {
+    alert("أدخل عدد الثواني");
+    return;
+  }
+
+  clearInterval(timer);
+
+  timer = setInterval(() => {
+
+    document.getElementById("timerResult").innerText =
+      remaining + " ثانية";
+
+    remaining--;
+
+    if (remaining < 0) {
+
+      clearInterval(timer);
+
+      document.getElementById("timerResult").innerText =
+        "⏰ انتهى الوقت";
+
+      alert("انتهى الوقت!");
+    }
+
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
