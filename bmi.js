f4ae86d4b951f64928b3760e4387e5410693f2d5
educationxwx-rgb <educationxwx@gmail.com>
2026-06-11 06:46:19 +0100

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,1 +1,33 @@
+function calculateBMI() {
 
+  let weight =
+    Number(document.getElementById("weight").value);
+
+  let height =
+    Number(document.getElementById("height").value);
+
+  if (!weight || !height) {
+
+    document.getElementById("bmiResult").innerText =
+      "أدخل الوزن والطول";
+
+    return;
+  }
+
+  let bmi = weight / (height * height);
+
+  let status = "";
+
+  if (bmi < 18.5) {
+    status = "نقص الوزن";
+  } else if (bmi < 25) {
+    status = "وزن طبيعي";
+  } else if (bmi < 30) {
+    status = "زيادة في الوزن";
+  } else {
+    status = "سمنة";
+  }
+
+  document.getElementById("bmiResult").innerText =
+    `BMI: ${bmi.toFixed(1)} - ${status}`;
+}
