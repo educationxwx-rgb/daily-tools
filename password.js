5c9498e001f09754fbb38601b2528e05e842b444
educationxwx-rgb <educationxwx@gmail.com>
2026-06-11 06:50:18 +0100

    Update password.js

--- password.js
+++ password.js
@@ -1,1 +1,33 @@
+function generate() {
 
+  let chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
+
+  let password = "";
+
+  for (let i = 0; i < 12; i++) {
+
+    password += chars.charAt(
+      Math.floor(Math.random() * chars.length)
+    );
+  }
+
+  document.getElementById("pass").innerText =
+    password;
+}
+
+function copyPassword() {
+
+  let text =
+    document.getElementById("pass").innerText;
+
+  if (!text) {
+
+    alert("قم بإنشاء كلمة مرور أولاً");
+    return;
+  }
+
+  navigator.clipboard.writeText(text);
+
+  alert("تم النسخ");
+}
